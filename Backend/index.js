6442d54f2718a5f1bd2185631e3932e94a376f67
Ethan Lae <elae0001@student.monash.edu>
2024-04-11 10:55:04 +1000

    adding comments

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
-//Import cors
+//Import express to access the http server
 const express = require("express");
+//Import cors to access server from anywhere
 const cors = require("cors");
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+  //Returns username and password
   return res.json({ username: username, secret: "sha256..." });
 });
 //Port is 3001
